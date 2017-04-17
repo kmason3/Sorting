de32c5814dabd0e31c100b8ac9a2b4a938f33f89
kurt <kurt@kurt-ThinkPad-T440>
2017-04-16 20:59:41 -0400

    Node Class

--- Homework5/src/sorting/Node.java
+++ Homework5/src/sorting/Node.java
@@ -1,5 +1,46 @@
 package sorting;
 
+
 public class Node {
+	private String data;
+	private Node next;
+
+	public Node() {
+
+		this.setData(null);
+		this.setNext(null);
+
+	}
+
+	public Node(String input) {
+
+		this.setData(input);
+		this.setNext(null);
+
+	}
+
+	public String getData() {
+		return data;
+	}
+
+	public void setData(String data) {
+		this.data = data;
+	}
+
+	public Node getNext() {
+		return next;
+	}
+
+	public void setNext(Node next) {
+		this.next = next;
+	}
+
+	public void print() {
+		System.out.println(this.data);
+
+	}
 
 }
+
+
+
