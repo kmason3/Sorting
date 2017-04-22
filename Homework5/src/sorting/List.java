6196702c3b1ff91452674bb2a0ad143239c24def
kurt <kurt@kurt-ThinkPad-T440>
2017-04-22 01:58:47 -0400

    add method that takes input of integer and adds to list

--- Homework5/src/sorting/List.java
+++ Homework5/src/sorting/List.java
@@ -1,67 +1,83 @@
 package sorting;
 
-
 public class List {
-	
-		private int size = 0;
-		private Node head = null;
 
-		
-		public List() {
+	private int size = 0;
+	private Node head = null;
 
-		}
-
-		
-		public boolean isEmpty() {
-			return size == 0;
-		}
-		
-		public void add(Node node) {
-			
-			if (isEmpty()) {
-
-				head = node;
-				size++;
-				return;
-			}
-
-			Node temp = head;
-
-			while (temp.getNext() != null) {
-				temp = temp.getNext();
-			}
-
-			temp.setNext(node);
-			size++;
-
-			
-
-		}
-
-		
-		public int size() {
-			return size;
-		}
-
-		public void print() {
-		
-			if (isEmpty()) {
-				System.out.println("List is empty");
-			}
-
-			Node temp = head;
-
-			while (temp != null) {
-
-				if (temp.getNext() != null) {
-					System.out.print(temp.getElement() + ",");
-				} else {
-					System.out.print(temp.getElement());
-					System.out.println();
-				}
-
-				temp = temp.getNext();
-			}
-		}
+	public List() {
 
 	}
+
+	public boolean isEmpty() {
+		return size == 0;
+	}
+
+	public void add(Node node) {
+
+		if (isEmpty()) {
+
+			head = node;
+			size++;
+			return;
+		}
+
+		Node temp = head;
+
+		while (temp.getNext() != null) {
+			temp = temp.getNext();
+		}
+
+		temp.setNext(node);
+		size++;
+
+	}
+
+	public void add(int input) {
+
+		Node node = new Node(input);
+
+		if (isEmpty()) {
+
+			head = node;
+			size++;
+			return;
+		}
+
+		Node temp = head;
+
+		while (temp.getNext() != null) {
+			temp = temp.getNext();
+		}
+
+		temp.setNext(node);
+		size++;
+
+	}
+
+	public int size() {
+		return size;
+	}
+
+	public void print() {
+
+		if (isEmpty()) {
+			System.out.println("List is empty");
+		}
+
+		Node temp = head;
+
+		while (temp != null) {
+
+			if (temp.getNext() != null) {
+				System.out.print(temp.getElement() + ",");
+			} else {
+				System.out.print(temp.getElement());
+				System.out.println();
+			}
+
+			temp = temp.getNext();
+		}
+	}
+
+}
