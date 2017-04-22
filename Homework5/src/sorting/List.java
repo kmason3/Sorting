384082f2e958b9886ef5ddf345e702fcdd1796be
kurt <kurt@kurt-ThinkPad-T440>
2017-04-22 02:30:52 -0400

    finished adding comments

--- Homework5/src/sorting/List.java
+++ Homework5/src/sorting/List.java
@@ -3,8 +3,8 @@ package sorting;
 import java.util.Random;
 
 public class List {
-	
-	//Private variables
+
+	// Private variables
 
 	private int size = 0;
 	private Node head = null;
@@ -13,23 +13,27 @@ public class List {
 	 * Empty Constructor
 	 */
 	public List() {
-		
+
 		head = null;
-		
+
 	}
+
 	/**
 	 * Checks is list is empty
+	 * 
 	 * @return boolean
 	 */
 	public boolean isEmpty() {
 		return size == 0;
 	}
+
 	/**
 	 * Adds node
+	 * 
 	 * @param node
 	 */
 	public void add(Node node) {
-		//if list is empty add node to front of list
+		// if list is empty add node to front of list
 		if (isEmpty()) {
 
 			head = node;
@@ -39,7 +43,7 @@ public class List {
 
 		// if list is not empty, adds to end of list
 		Node current = head;
-		
+
 		while (current.getNext() != null) {
 			current = current.getNext();
 		}
@@ -51,35 +55,37 @@ public class List {
 
 	/**
 	 * adds node with int input
+	 * 
 	 * @param input
 	 */
 	public void add(int input) {
-		//creates 
+		// creates new node with element of the integer input
 		Node node = new Node(input);
-		
+		// adds new node to list
 		this.add(node);
-		
 
 	}
-	
+
 	/**
 	 * adds random number between 1 and 50
 	 */
 	public void addRandom() {
+		// new random variable
 		Random rand = new Random();
-
-		int n = rand.nextInt(50) + 1;
-
-		this.add(n);
+		// variable for a random integer between 1 and 50
+		int randomNumber = rand.nextInt(50) + 1;
+		// adds random number to list
+		this.add(randomNumber);
 	}
-	
+
 	/**
 	 * fills list to user defined size
+	 * 
 	 * @param total
 	 */
 
 	public void fillList(int listSize) {
-
+		// adds random numbers to list until size reaches the user defined size
 		for (int i = 1; i <= listSize; i++) {
 
 			this.addRandom();
@@ -92,17 +98,26 @@ public class List {
 	 * sorts list using bubble sort algorithm
 	 */
 	public void bubbleSort() {
+		// if list is empty prints list is empty
 		if (isEmpty()) {
 			System.out.println("List is Empty!");
 		}
+		// if list has one element prints list has one element and is sorted
 		if (size == 1) {
 			System.out
 					.println("List only contains one item, so, you know, it's already sorted!");
 		}
 
+		// variables for if something has been swapped and current node
 		boolean swapped = true;
 		Node current = this.head;
-
+		/*
+		 * if an item has been swapped or it's the first iteration enters loop
+		 * while the current element is not null checks if the current element
+		 * is larger than the next if it is swaps them then goes to next node
+		 * and test the element until no items are swapped then the list is
+		 * sorted!
+		 */
 		while (swapped) {
 
 			swapped = false;
@@ -127,6 +142,10 @@ public class List {
 		}
 	}
 
+	/**
+	 * 
+	 * @return list size
+	 */
 	public int size() {
 		return size;
 	}
@@ -135,11 +154,12 @@ public class List {
 	 * prints list
 	 */
 	public void print() {
-
+		// if empty print list is empty
 		if (isEmpty()) {
 			System.out.println("List is empty");
 		}
-
+		// if list is not empty prints out all elements separated by commas
+		// except the last, which has no comma
 		Node current = head;
 
 		while (current != null) {
