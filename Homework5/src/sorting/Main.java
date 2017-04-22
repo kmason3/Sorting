1306372dff2ccffa2c8db97f45e0fb793866889b
kurt <kurt@kurt-ThinkPad-T440>
2017-04-22 02:06:36 -0400

    tests bubbleSort()

--- Homework5/src/sorting/Main.java
+++ Homework5/src/sorting/Main.java
@@ -4,12 +4,13 @@ public class Main {
 
 	public static void main(String[] args) {
 		List test = new List();
-
+		System.out.println("Unsorted:");
 		test.fillList(10);
 
 		test.print();
 
-		test.fillList(20);
+		test.bubbleSort();
+		System.out.println("Sorted:");
 		test.print();
 	}
 
