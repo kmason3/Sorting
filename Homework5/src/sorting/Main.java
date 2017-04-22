ce8f8decd55266fb617f6bf96139eb5c313e77e2
kurt <kurt@kurt-ThinkPad-T440>
2017-04-22 01:59:31 -0400

    tests integer add function

--- Homework5/src/sorting/Main.java
+++ Homework5/src/sorting/Main.java
@@ -5,13 +5,10 @@ public class Main {
 	public static void main(String[]args){
 		List test = new List();
 		
-		Node nodeTest = new Node(5);
-		Node nodeTest2 = new Node(6);
-		Node nodeTest3 = new Node(4);
-		
-	test.add(nodeTest);
-	test.add(nodeTest2);
-	test.add(nodeTest3);
+		test.add(5);
+		test.add(6);
+		test.add(7);
+	
 	
 	test.print();
 	}
