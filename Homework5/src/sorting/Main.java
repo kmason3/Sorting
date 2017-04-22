674b5123f0fab7eff83a0da638c0db123af689eb
kurt <kurt@kurt-ThinkPad-T440>
2017-04-22 02:01:17 -0400

    test for addRandom()

--- Homework5/src/sorting/Main.java
+++ Homework5/src/sorting/Main.java
@@ -5,9 +5,9 @@ public class Main {
 	public static void main(String[]args){
 		List test = new List();
 		
-		test.add(5);
-		test.add(6);
-		test.add(7);
+		test.addRandom();
+		test.addRandom();
+		test.addRandom();
 	
 	
 	test.print();
